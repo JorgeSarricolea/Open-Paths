9ffad11782c4ff4a98699f22b68b8a719631d73a
Jorge Sarricolea <jjorgesarricolea18@gmail.com>
2023-04-12 18:12:51 -0600

    Version 1.3

--- open-paths.py
+++ open-paths.py
@@ -5,12 +5,11 @@ menu = """
     -- MENU OF PATHS --
 
     1. Portfolio
-    2. Personal folder
     2. ITM Folder
     3. Projects
     4. Downloads
     5. Multimedia
-    6. open the first 3 paths
+    6. Open the first 3 paths
     7. Exit
         """
 option = 0
@@ -19,7 +18,6 @@ activeMenu=1
 #Paths
 portfolio = "C:/Users/jorge/OneDrive - INSTITUTO TECNOLOGICO DE MERIDA\Documents\Projects\Jorge Sarricolea\GitHub\Portfolio 2.0"
 itmPath = "C:/Users/jorge/OneDrive - INSTITUTO TECNOLOGICO DE MERIDA\Instituto Tecnológico de Mérida"
-personalFolder = "C:/Users/jorge/OneDrive - INSTITUTO TECNOLOGICO DE MERIDA\Documents\Projects\Jorge Sarricolea"
 projects = "C:/Users/jorge/OneDrive - INSTITUTO TECNOLOGICO DE MERIDA\Documents\Projects"
 downloads = "C:/Users/jorge/Downloads"
 multimedia = "C:/Users/jorge/OneDrive - INSTITUTO TECNOLOGICO DE MERIDA\Multimedia"
@@ -30,20 +28,18 @@ while activeMenu == 1:
     if option == 1:
         os.startfile(portfolio)
     if option == 2:
-        os.startfile(personalFolder)
+        os.startfile(itmPath)
     if option == 3:
-        os.startfile(itmPath)
+        os.startfile(projects)
     if option == 4:
-        os.startfile(projects)
-    if option == 5:
         os.startfile(downloads)
-    if option == 6:
+    if option == 5:
         os.startfile(multimedia)
-    if option == 7:
-        os.startfile(personalFolder)
+    if option == 6:
+        os.startfile(portfolio)
         os.startfile(itmPath)
         os.startfile(projects)
-    if option == 8:
+    if option == 7:
         activeMenu = 0
         print("\nBYE BYE 👋")
 
